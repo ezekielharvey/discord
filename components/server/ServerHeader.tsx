db833a6e7bf88303bdb832e5e5e80c31a8c39a92
ezekielharvey <zekeharvey@outlook.com>
2023-10-13 14:10:14 -0400

    Delete and Leave Modal completed

--- components/server/ServerHeader.tsx
+++ components/server/ServerHeader.tsx
@@ -77,6 +77,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
         {isModerator && (
           <DropdownMenuItem
+            onClick={() => onOpen("deleteServer", { server })}
             className='text-rose-500 px-3 py-2 text-sm cursor-pointer'
           >
             Delete Server
@@ -87,6 +88,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
         {!isAdmin && (
           <DropdownMenuItem
+          onClick={() => onOpen("leaveServer", { server })}
             className='text-rose-500 px-3 py-2 text-sm cursor-pointer'
           >
             Leave Server
