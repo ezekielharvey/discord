538467b5a7794f0c45b507a165c8aeb280f5c549
ezekielharvey <zekeharvey@outlook.com>
2023-10-13 09:03:40 -0400

    Invitations completed

--- components/server/ServerHeader.tsx
+++ components/server/ServerHeader.tsx
@@ -84,7 +84,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
         {!isAdmin && (
           <DropdownMenuItem
-            className='px-3 py-2 text-sm cursor-pointer'
+            className='text-rose-500 px-3 py-2 text-sm cursor-pointer'
           >
             Leave Server
             <LogOut 
